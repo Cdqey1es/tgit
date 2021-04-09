4b1b320c54793c5dce5dda9d200e3ebc423f3dbe
Cdqey1es <shuanao3459b@126.com>
2021-04-09 19:56:19 +0800

    a

---
+++ go.go
@@ -0,0 +1,19 @@
+package main
+
+import "os/exec"
+import "fmt"
+
+func redShow(err string) {
+	fmt.Printf("%c[1;40;31m%s%c[0m\n", 0x1B, err, 0x1B)
+}
+
+func main() {
+	cmd := exec.Command("ssh-keygen", "-lf", "/tmp/497845385/id_rsa.pub")
+	// cmd := exec.Command("ls", "-l", "/tmp/afasdfa")
+	if err := cmd.Start(); err != nil {
+		redShow(err.Error())
+	} else {
+		err = cmd.Wait()
+		fmt.Println("--ok", err.Error())
+	}
+}
