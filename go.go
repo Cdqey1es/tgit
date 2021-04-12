ec5f1e21b0811cc328aedefb2eb02f37e1256709
Cdqey1es <shuanao3459b@126.com>
2021-04-12 11:06:34 +0800

    a

--- go.go
+++ go.go
@@ -3,6 +3,8 @@ package main
 import "os/exec"
 import "fmt"
 import "time"
+import "os"
+import "io/ioutil"
 
 func redShow(err string) {
 	fmt.Printf("%c[1;40;31m%s%c[0m\n", 0x1B, err, 0x1B)
@@ -20,9 +22,22 @@ func tssh() {
 		}
 	}
 }
+func tread() {
+	f, err := os.Open("/tmp/497845385/id_rsa.pub")
+	if err != nil {
+		fmt.Println("open:", err.Error())
+	}
+	b, err := ioutil.ReadAll(f)
+	if err != nil {
+		fmt.Println("read:", err.Error())
+	} else {
+		fmt.Println(string(b)[0:10])
+	}
+}
 func main() {
 	for idx := 0; idx != 10000; idx++ {
 		time.Sleep(time.Second * 1)
 		tssh()
+		tread()
 	}
 }
